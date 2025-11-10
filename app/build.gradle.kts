dbe9eaaa98c6e9d12c9f1e9cc2b03f96b50c63d4
ashishsince94-cloud <ashish.since94@gmail.com>
2025-11-10 15:01:04 +0530

    Added with new style
    Added with new style

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,5 +82,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    androidTestImplementation("io.mockk:mockk:1.13.13")
+    androidTestImplementation(libs.mockk)
 }
